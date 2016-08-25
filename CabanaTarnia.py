9c78f417b0ee56fdaa8fa5f9cb51ebafbbc7d438
iboros1 <iborosppc@gmail.com>
2016-08-25 23:05:40 +0300

    first page

--- CabanaTarnia.py
+++ CabanaTarnia.py
@@ -3,10 +3,46 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!!!MEGY'
+def index(): pass
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login')
+def login(): pass
+
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+
+with app.test_request_context():
+    print(url_for('index'))
+print url_for('login')
+print url_for('login', next='/')
+print url_for('profile', username='John Doe')
+
+
+login
+login?next=/
+user/John%20Doe
+
+
+# @app.route('/')
+# def index():
+#     return 'Index Page'
+#
+#
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+#
+#
+# @app.route('/Cabana/')
+# def projects():
+#     return 'The project page'
+#
+#
+# @app.route('/about')
+# def about():
+#     return 'The about page'
