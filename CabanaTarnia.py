588a69215d3d1954b6183d11b7ebafac3a1fcb0d
iboros <iboros1@users.noreply.github.com>
2016-08-26 10:22:22 +0300

    Update CabanaTarnia.py

--- CabanaTarnia.py
+++ CabanaTarnia.py
@@ -18,14 +18,11 @@ def profile(username): pass
 
 with app.test_request_context():
     print(url_for('index'))
-print url_for('login')
-print url_for('login', next='/')
-print url_for('profile', username='John Doe')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='John Doe')
 
 
-login
-login?next=/
-user/John%20Doe
 
 
 # @app.route('/')
